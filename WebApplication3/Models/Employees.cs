c0c2c8b19740e153812bbc0f2c0e87cb1a3b1c3c
Belg <borisovbelg@icloud.com>
2022-08-03 20:51:48 +0300

    Added fields

--- WebApplication3/Models/Employees.cs
+++ WebApplication3/Models/Employees.cs
@@ -2,5 +2,14 @@
 {
     public class Employees
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Patronymic { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Mail { get; set; }
+        public string? Password { get; set; }
+
+        public int? DepartmentId { get; set; }
     }
 }
--- WebApplication3/Models/SubDepartament.cs
+++ WebApplication3/Models/SubDepartament.cs
@@ -2,5 +2,11 @@
 {
     public class SubDepartament
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+
+        public int ParentDepartmentId { get; set; }
+
     }
 }
