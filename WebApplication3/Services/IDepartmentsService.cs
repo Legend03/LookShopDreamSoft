e9bebe7196c2768e1be046842d2f02d1bea88846
Alexandr Lukanev <lukanevteach@gmail.com>
2022-08-06 22:56:07 +0300

    Added IDepartmentsService

---
+++ WebApplication3/Services/IDepartmentsService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication3.Data;
+using WebApplication3.Models;
+
+namespace WebApplication3.Services
+{
+    public interface IDepartmentsService
+    {
+        Task<IEnumerable<Departments?>> GetDepartments();
+        Task<Departments?> GetById(int departmentId);
+        Task Create(Departments department);
+        Task Update(Departments department);
+        Task Remove(int departmentId);
+    }
+
+    public class DepartmentsService : IDepartmentsService
+    {
+        private readonly AppDbContext _db;
+
+        public DepartmentsService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IEnumerable<Departments?>> GetDepartments() =>
+            await _db.Departments.ToListAsync();
+
+        public async Task<Departments?> GetById(int departmentId) =>
+            await _db.Departments.AsNoTracking().FirstOrDefaultAsync(u => u.Id == departmentId);
+
+
+        public async Task Create(Departments department)
+        {
+            await _db.Departments.AddAsync(department);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Update(Departments department)
+        {
+            _db.Departments.Update(department);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Remove(int departmentId)
+        {
+            _db.Departments.Remove(new Departments { Id = departmentId });
+            await _db.SaveChangesAsync();
+        }
+    }
+}
